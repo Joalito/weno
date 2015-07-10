c0ffb248bcec3cd25a0f0a90e9e6c7a43a9e5098
Joalito <joseph.balsells@yale.edu>
2015-07-10 12:21:34 -0600

    Update Values.py

--- Values.py
+++ Values.py
@@ -1,18 +1,7 @@
-import numpy as np
-
-class BasicState:
-
-    def __init__(self, grid):
-        #self.u_momentum = np.sin(2*np.pi*grid.x/grid.lx)**3
-        #self.u_momentum = np.sin(2*np.pi*grid.x/grid.lx)**3 + 3
-        self.u_momentum = np.sin(2*np.pi*grid.x/grid.lx)**2
-        #self.u_momentum = np.asarray(delta)
-
-nx = 150
-delta = []
-for _ in range(nx/3):
-    delta.append(0)
-for _ in range(nx/3):
-    delta.append(1)
-for _ in range(nx/3):
-    delta.append(0)
+class Namelist:
+    def __init__(self):
+        self.nx = 150
+        self.ny = 50
+        self.dx = 1.0
+        self.dy = 1.
+        self.cfl = .3
